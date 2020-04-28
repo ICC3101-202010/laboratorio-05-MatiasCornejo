8d09b0f7805033553d80148c18a9d0500ef20ad1
Matias  Cornejo lazcano <matiascornejolazcano@MacBook-Air-de-Matias.local>
2020-04-28 00:10:22 -0400

    firs commit

---
+++ Lab5Poo/Lab5Poo/User.cs
@@ -0,0 +1,52 @@
+﻿using System;
+namespace Lab5Poo
+{
+    public class User
+    {
+        public int EmailVerified { get; internal set; }
+
+
+        //aqui creare el evento de EmailVerified
+
+        // Paso 1: Creamos el delegate para el evento de verificar mail
+        public delegate void EmailVerifiEventHandler(object source, EventArgs args);
+        // Paso 2: Creamos el evento que se engatilla cuando el usuario se registra
+        public event EmailVerifiEventHandler EmailVerifi;
+
+        protected virtual void OnEmailVerified(User user, EventArgs args)
+        {
+            OnEmailVerified(this, new EventArgs());  //lanza el evento despues del correo de confirmacion
+
+        }
+        //crearemos el metodo
+
+
+
+        public void OnEmailSent(object source,EventArgs args)
+
+
+
+
+        {
+            
+            Console.WriteLine("¿Desea verificar el correo ?\n\n");
+            Console.WriteLine("1) SI");
+
+            Console.WriteLine("2)NO");
+
+            string a = Console.ReadLine();
+            int num = Convert.ToInt32(a);
+            if (num == 1)
+            {
+                
+                OnEmailVerified(this,new EventArgs());
+                
+            }
+           
+            
+
+        }
+
+        
+    }
+}
